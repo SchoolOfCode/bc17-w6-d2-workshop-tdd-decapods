675a4fcd4cdf4d22f31ef102de4c542266a6a23b
Crom <daffodil.luck9@gmail.com>
2024-08-06 14:04:00 +0100

    fail test.each for letter A to C

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,16 +1,14 @@
 import { calculateScrabbleScore } from "./scrabble-score";
 import { test, expect } from "vitest";
 
-test('A score test', () => {
-    expect(calculateScrabbleScore(`A`)).toBe(1)
-})
+test("A score test", () => {
+	expect(calculateScrabbleScore(`A`)).toBe(1);
+});
 
 test.each([
-    {a: "A", expected: 1},
-    {a: "B", expected: 3},
-    {a: "C", expected: 3},
-])(calculateScrabbleScore (%s)) => {
-    expect(a).toBe(expected)
-}
-
-)
+	{ a: "A", expected: 1 },
+	{ a: "B", expected: 3 },
+	{ a: "C", expected: 3 },
+])("calculateScrabbleScore ($a)) -> $expected ", ({ a, expected }) => {
+	expect(b).toBe(expected);
+});
